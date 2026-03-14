819e94c7576816a1a5ed29a22f68c3698d996800
Kelly Sabino <ksabino73@gmail.com>
2026-03-14 15:32:26 -0300

    ProjetosJava

---
+++ TesteJava/Teste1.java
@@ -0,0 +1,10 @@
+public class Teste1 {
+    public class Aluno{
+        public static void main(String[] args){
+            System.out.println("Aluno(a): Kelly");
+            System.out.println("Curso: Licenciatura em Computação");
+            System.out.println("Faculdade: IFPI");
+            
+        }
+    }
+}
---
+++ TesteJava/Teste3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Teste3 {
+            public static void main(String[] args){
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Digite o base: ");
+            double base = scanner.nextDouble();
+            
+            System.out.println("Digite a altura: ");
+            double altura = scanner.nextDouble();
+
+            double area = (base*altura) /2;
+            System.out.println("Area é = " + area);
+
+            
+        }
+
+    
+}
